b15135282903040b97f3a76d4806c3b980296bf3
Eliassbjunior <faculiff2017.2@gmail.com>
2022-02-17 16:31:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const client = new Client({
 	intents: [
 		
 	]
-})
+});
 
 client.once('ready', function () {
 	console.log('Online');
 })
 
 client.on('messageCreate', function (message) {
-    if(message.content === "oi") message.reply("Oi, funcionou!");
+    if(message.content === "oi"){ message.reply("Oi, funcionou!");}
 })
+
 client.login(process.env.BOT_TOKEN);
